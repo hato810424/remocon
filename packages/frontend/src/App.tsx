d940f4b6baaf09a326faf7fc35dd95112caf0a19
hato810424 <yukkuri.hatopoppo@gmail.com>
2025-04-11 02:48:17 +0900

    feat: Hono RPCのClientを追加

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { hc } from 'hono/client'
+import { BackendAppType } from '@remocon/backend'
+
+const client = hc<BackendAppType>(location.origin)
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    client.api.health.$get().then((res) => {
+      console.log(res)
+    })
+  }, []);
 
   return (
     <>
