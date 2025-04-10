387ce9db49631c39d3fea9d51f90dd2ab61d1b42
hato810424 <yukkuri.hatopoppo@gmail.com>
2025-04-11 03:43:08 +0900

    feat: OBSのURLをenvから変更できるように

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -3,6 +3,10 @@ import { logger } from "hono/logger";
 import { poweredBy } from "hono/powered-by";
 import { OBSController } from "./utils/obs-controller";
 import apiRouter from "./api";
+import dotenv from "dotenv";
+import { resolve } from "path";
+
+dotenv.config({ path: resolve(import.meta.dirname, "../../../.env") });
 
 export type Variables = {
   obsController: OBSController;
@@ -14,7 +18,7 @@ app.use("*", logger());
 app.use("*", poweredBy());
 
 // OBSControllerを初期化
-const obsController = new OBSController({ url: "ws://192.168.10.110:4455" });
+const obsController = new OBSController({ url: process.env.OBS_URL || "ws://localhost:4455" });
 obsController.connect();
 app.use("*", async (c, next) => {
   c.set("obsController", obsController);
